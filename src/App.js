41e985b5bac6ad5fa123fffce9c392f427ef9883
Nihar Sawant <nihar.sawant@businessnext.com>
2023-03-07 16:48:40 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
+import Footer from './components/Footer';
+import Mainframe from './components/Mainframe';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The lazy fox jumped over the dog</h1>
+      <Navbar/>
+      <Mainframe/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Mainframe.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mainframe = () => {
+  return (
+    <div>
+      <h1>Mainframe Component</h1>
+    </div>
+  )
+}
+
+export default Mainframe
